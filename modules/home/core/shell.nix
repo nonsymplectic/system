6949137d39e24ade9871cccb096e7794545ee9d4
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-02-01 11:28:45 +0100

    shell final

--- modules/home/core/shell.nix
+++ modules/home/core/shell.nix
@@ -1,12 +1,15 @@
 { ... }:
-
 {
   programs.bash = {
     enable = true;
 
-    # Override the distro default prompt (which starts with '\n')
-    promptInit = ''
-      PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
+    # ~/.bashrc content; guard so it only affects interactive shells.
+    bashrcExtra = ''
+      case $- in
+        *i*)
+          PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
+          ;;
+      esac
     '';
   };
 }
