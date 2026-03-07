f6d777532337cef6b99d01320e01aa48ba48112c
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-03-07 10:24:31 +0100

    add bluetooth

--- hosts/home-pc/hardware.nix
+++ hosts/home-pc/hardware.nix
@@ -1,5 +1,24 @@
 { config, lib, pkgs, ... }:
 {
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = false;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+
   # OpenGL / graphics stack
   hardware.graphics.enable = true;
 
--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -3,6 +3,7 @@
   home.packages =
     (with pkgs; [
       # --- GUI ---
+      blueman
       chromium
       qutebrowser
       keepassxc
