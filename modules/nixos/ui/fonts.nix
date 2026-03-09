a234af3e670019b0e7ae50f538600a12cfdd529e
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-03-09 11:17:54 +0100

    more fonts

--- modules/nixos/ui/fonts.nix
+++ modules/nixos/ui/fonts.nix
@@ -16,7 +16,16 @@
   ];
 
   # Make fonts discoverable system-wide via fontconfig.
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig =
+    {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "Noto Sans" "Noto Sans CJK SC" "Noto Sans CJK TC" "Noto Sans CJK JP" "Noto Sans CJK KR" ];
+        serif = [ "Noto Serif" "Noto Serif CJK SC" "Noto Serif CJK TC" "Noto Serif CJK JP" "Noto Serif CJK KR" ];
+        monospace = [ "Noto Sans Mono CJK SC" "Noto Sans Mono CJK TC" "Noto Sans Mono CJK JP" "Noto Sans Mono CJK KR" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
 
   # Optional but commonly useful: exposes a font directory in the system profile.
   fonts.fontDir.enable = true;
