f5a558b21a93a5caea850540e0fcfa4a376c570e
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-03-24 17:57:39 +0100

    .

--- features/apps/viewers.nix
+++ features/apps/viewers.nix
@@ -3,13 +3,10 @@
 {
   config,
   lib,
-  pkgs,
   ...
-}:
-let
+}: let
   cfg = config.features.viewers;
-in
-{
+in {
   options.features.viewers = {
     enable = lib.mkEnableOption "Media viewer applications";
 
