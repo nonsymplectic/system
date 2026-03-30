e0c6814ce4909f5d973ffb08dc9fe8701337167c
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-03-30 17:12:15 +0200

    Update tuigreet.nix

--- features/system/tuigreet.nix
+++ features/system/tuigreet.nix
@@ -1,10 +1,18 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  greeting = ''
+     _      _  ____ _     ____  _       _  ____ _____    _  __ _  _      _____
+    / \__/|/ \/   _Y \ /|/  _ \/ \     / \/ ___Y__ __\  / |/ // \/ \  /|/  __/
+    | |\/||| ||  / | |_||| / \|| |     | ||    \ / \    |   / | || |\ ||| |  _
+    | |  ||| ||  \_| | ||| |-||| |_/\  | |\___ | | |    |   \ | || | \||| |_//
+    \_/  \|\_/\____|_/ \|\_/ \|\____/  \_/\____/ \_/    \_|\_\\_/\_/  \|\____\
+  '';
+in {
   services.greetd = {
     enable = true;
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway --remember --greeting 'Michal ist King' --asterisks --asterisks-char '#'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway --remember --greeting '${greeting}' --asterisks --asterisks-char '*'";
         user = "greeter";
       };
     };
