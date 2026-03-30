d8924ab862aecd6c893227308692bb2e04c15093
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-03-30 16:52:20 +0200

    Update tuigreet.nix

--- features/system/tuigreet.nix
+++ features/system/tuigreet.nix
@@ -4,7 +4,7 @@
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway --remember --greeting 'Michal ist King'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway --remember --greeting 'Michal ist King' --asterisks --asterisks-char '#'";
         user = "greeter";
       };
     };
