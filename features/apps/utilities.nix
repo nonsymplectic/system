ae1a9b47550618b74f2ec975b73499460c6606d0
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-04-01 15:37:09 +0200

    Update utilities.nix

--- features/apps/utilities.nix
+++ features/apps/utilities.nix
@@ -22,15 +22,23 @@ in {
       default = true;
       description = "Enable AnyDesk remote desktop";
     };
+
+    udiskie = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable udisk/udiskie disk tools";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     home-manager.sharedModules = [
       {
         home.packages =
-          (lib.optionals cfg.blueman [pkgs.blueman])
-          ++ (lib.optionals cfg.anydesk [pkgs.anydesk]);
+          (lib.optionals cfg.blueman [pkgs.blueman]) ++ (lib.optionals cfg.anydesk [pkgs.anydesk]);
+
+        services.udiskie.enable = cfg.udiskie;
       }
     ];
+    services.udisks2.enable = cfg.udiskie;
   };
 }
