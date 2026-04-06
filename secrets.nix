7230dbd959bb3f8f426017df5e1fcf8472f4ea49
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-04-06 15:15:39 +0200

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,9 +1,21 @@
 let
   home-pc = "age1ut4vhgwvnkng46zx0et7a7ptcfaxpsgjj94mrqmkzl7z706dafrqzklrxx"; # home-pc public key
-  #  laptop = "age1...";   # paste laptop public key
+  laptop = "age1y4esvm73q2lgvf29dkzvs8plkj0dm0pxw605k4k9u6zu89ps6ags0qq929"; # laptop public key
 in {
-  "secrets/github_id_ed25519_github.age".publicKeys = [home-pc]; #don't forget to add laptop later
-  "secrets/borgbackup_passphrase.age".publicKeys = [home-pc]; # Shared passphrase for borg repository
-  "secrets/borgbackup_b2_env.age".publicKeys = [home-pc]; # Backblaze B2 credentials
-  "secrets/rclone_conf.age".publicKeys = [home-pc]; # Rclone configuration for B2
+  "secrets/github_id_ed25519_github.age".publicKeys = [
+    home-pc
+    laptop
+  ]; # don't forget to add laptop later
+  "secrets/borgbackup_passphrase.age".publicKeys = [
+    home-pc
+    laptop
+  ]; # Shared passphrase for borg repository
+  "secrets/borgbackup_b2_env.age".publicKeys = [
+    home-pc
+    laptop
+  ]; # Backblaze B2 credentials
+  "secrets/rclone_conf.age".publicKeys = [
+    home-pc
+    laptop
+  ]; # Rclone configuration for B2
 }
