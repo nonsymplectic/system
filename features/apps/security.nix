afc393ed96b7a3e81ce9e329d5c791122e4af8d9
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-04-23 18:11:53 +0200

    gnupg

--- features/apps/security.nix
+++ features/apps/security.nix
@@ -16,12 +16,19 @@ in {
       default = true;
       description = "Enable KeePassXC password manager";
     };
+    gnupg = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable GnuPG OpenPGP";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     home-manager.sharedModules = [
       {
-        home.packages = lib.optionals cfg.keepassxc [pkgs.keepassxc];
+        home.packages =
+          (lib.optionals cfg.keepassxc [pkgs.keepassxc])
+          ++ (lib.optionals cfg.gnupg [pkgs.gnupg]);
       }
     ];
   };
