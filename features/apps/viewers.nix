455d877796fe90d5b20d60771c435aa586da9053
Michał Mikuta <nonsymplectic@users.noreply.github.com>
2026-06-08 11:34:41 +0200

    epr as default epub viewer

--- features/apps/viewers.nix
+++ features/apps/viewers.nix
@@ -11,7 +11,7 @@ in {
   options.features.viewers = {
     enable = lib.mkEnableOption "Media viewer applications";
 
-    enableSpotify = lib.mkOption {
+    spotify = lib.mkOption {
       type = lib.types.bool;
       default = true;
       description = "Include Spotify-Player";
@@ -25,25 +25,46 @@ in {
       default = "zathura";
       description = "Default PDF viewer for XDG MIME associations";
     };
+
+    defaultEpubViewer = lib.mkOption {
+      type = lib.types.enum [
+        "epr"
+        "none"
+      ];
+      default = "epr";
+      description = "Default Epub viewer for XDG MIME associations";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     home-manager.sharedModules = [
       {
+        # non HM
+        home.packages = with pkgs; [vlc epr];
+
         # Document viewer
         programs.zathura.enable = true;
 
+        # epr epub reader needs its own .desktop entry
+        xdg.desktopEntries.epr = {
+          name = "EPR";
+          genericName = "EPUB Reader";
+          comment = "Read EPUB books in the terminal with EPR";
+          exec = "${pkgs.foot}/bin/foot -T epr ${pkgs.epr}/bin/epr %f";
+          terminal = false; # Foot will handle the terminal
+          mimeType = ["application/epub+zip"];
+          categories = ["Office" "Viewer"];
+        };
+
         # Video player
         programs.mpv.enable = true;
-
-        home.packages = [pkgs.vlc];
 
         # Image viewers
         programs.imv.enable = true;
 
         # Spotify
         programs.spotify-player = {
-          enable = cfg.enableSpotify;
+          enable = cfg.spotify;
           settings = {
             notify_timeout_in_secs = 5;
             enable_audio_visualization = true;
@@ -55,14 +76,16 @@ in {
           };
         };
 
-        # home.packages = with pkgs; [
-        #   swayimg # Wayland-native image viewer
-        # ];
+        # Set MIME association
+        xdg.mimeApps.defaultApplications = lib.mkMerge [
+          (lib.mkIf (cfg.defaultPdfViewer == "zathura") {
+            "application/pdf" = "org.pwmt.zathura.desktop";
+          })
 
-        # Set PDF MIME association
-        xdg.mimeApps.defaultApplications = lib.mkIf (cfg.defaultPdfViewer == "zathura") {
-          "application/pdf" = "org.pwmt.zathura.desktop";
-        };
+          (lib.mkIf (cfg.defaultEpubViewer == "epr") {
+            "application/epub+zip" = "epr.desktop";
+          })
+        ];
       }
     ];
   };
